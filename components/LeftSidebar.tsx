9d2c1f875e8cbc15de1828b45889d6dba0a0c0ad
Mohammad Haider <arzhdr@gmail.com>
2024-06-11 13:44:13 -0400

    updated leftsidebar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -6,9 +6,29 @@ const LeftSidebar = () => {
   return (
     <section className="left_sidebar">
       <nav className="flex flex-col gap-6">
-        <Link href="/">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-centers"
+        >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
+          <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">
+            Podders
+          </h1>
         </Link>
+        {[
+          {
+            route: "/profile",
+            label: "Profile",
+            imgUrl: "/icons/microphone.svg",
+          },
+          {
+            route: "/home",
+            label: "Home",
+            imgUrl: "/icons/home.svg",
+          },
+        ].map(({ route, label }) => {
+          return <Link href={route}>{label}</Link>;
+        })}
       </nav>
     </section>
   );
