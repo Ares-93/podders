6237096ea85ca66d4912c87e6005e79dd914a69c
Mohammad Haider <arzhdr@gmail.com>
2024-06-11 14:23:39 -0400

    added effect to active routes for smoother navigation

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -32,7 +32,10 @@ const LeftSidebar = () => {
               href={route}
               key={label}
               className={cn(
-                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start"
+                "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
+                {
+                  "bg-nav-focus border-r-4  border-red-1": isActive,
+                }
               )}
             >
               <Image src={imgURL} alt={label} width={24} height={24} />
