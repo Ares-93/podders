c519f653a7c6926de28656efa4274d46955a2277
Mohammad Haider <arzhdr@gmail.com>
2024-06-20 01:15:48 -0400

    made the design of the podcast cards more compact and more responsive

--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -15,7 +15,13 @@ const PodcastCard = ({
   return (
     <div className="cursor-pointer">
       <figure className="flex flex-col gap-2">
-        <Image src={imgURL} width={174} height={174} alt={title} />
+        <Image
+          src={imgURL}
+          width={174}
+          height={174}
+          alt={title}
+          className="aspect-square h-fit w-full rounded-xl 2xl:size-[200px]"
+        />
         <div className="flex flex-col">
           <h1
             className="text-16 truncate
